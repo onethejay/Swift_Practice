c8252abc7bbf8d72275dcbfbae234a34d27a19f0
onethejay <jhjang951@gmail.com>
2023-06-27 22:01:51 +0900

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by onethejay on 2023/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Hello, Jay!")
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by onethejay on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
