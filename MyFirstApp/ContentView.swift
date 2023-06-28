60d530a457e81498bcb461b6ad5bc1ce0b0f695a
onethejay <jhjang951@gmail.com>
2023-06-28 23:13:15 +0900

    Button 실습

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Button {
-//            print("Hited2")
-//        } label: {
-//            Text("Jay")
-//                .padding()  //텍스트에 패딩을 준다
-//                .frame(width: 150)  //너비를 150으로 지정
-//                .background(.purple)    //패딩이 있는 부분까지 배경색을 지정
-//                .cornerRadius(13)   //각 모서리를 동그랗게 만든다
-//        }
-
-        Button("Delete", role: .destructive) {
-            print("deleted!")
+        VStack {
+            Text("Hello Jay")
+                .bold()
+                .italic()
+                .strikethrough()
+            Text("Hello Jay")
+                .font(.system(size: 60))
+            Text("Hello Jay")
+                .underline(true, color: .orange)
+                .foregroundColor(.red)
+                .background(.purple)
+            Text("Hello Jay")
+                .foregroundColor(.green)
+                .bold()
+                .font(.system(size: 39, weight: .bold, design: .serif))
         }
     }
 }
