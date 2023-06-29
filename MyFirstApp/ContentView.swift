44da17266b66c036731cbf77749f62262deff918
onethejay <jhjang951@gmail.com>
2023-06-29 22:22:14 +0900

    Image 실습

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -2,14 +2,27 @@
 //  ContentView.swift
 //  MyFirstApp
 //
-//  Created by onethejay on 2023/06/27.
+//  Created by onethejay on 2023/06/28.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("swift-og")
+//        Image("swift-og")
+//            .resizable()
+//            .aspectRatio(contentMode: .fill)
+//            .frame(width: 200, height: 200)
+//            .clipped()
+//            .background(.orange)
+//            .border(.blue, width: 7)
+        
+        //SF symbol 에서 검색해서 가져올수 있다.
+        Image(systemName: "sun.min")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 50, height: 50)
+
     }
 }
 
