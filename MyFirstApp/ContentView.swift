2e3f3459af9690e550485249d8407bb340a8f44c
onethejay <jhjang951@gmail.com>
2023-06-29 22:34:35 +0900

    Stack 실습

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Image("swift-og")
-//            .resizable()
-//            .aspectRatio(contentMode: .fill)
-//            .frame(width: 200, height: 200)
-//            .clipped()
-//            .background(.orange)
-//            .border(.blue, width: 7)
-        
-        //SF symbol 에서 검색해서 가져올수 있다.
-        Image(systemName: "sun.min")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 50, height: 50)
-
+        ZStack() {
+            //안에는 10개만 들어갈 수 있다.
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.blue)
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.blue)
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.blue)
+            Text("1")
+            
+        }
+        .frame(width: 300, height: 200)
+        .background(.orange)
     }
 }
 
